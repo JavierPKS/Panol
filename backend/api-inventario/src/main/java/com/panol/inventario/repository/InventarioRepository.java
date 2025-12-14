19ff54faabf2e6e38332845207c7aaf5e667a7fc
Javier <javierparrareyes12345@gmail.com>
2025-12-14 14:48:57 -0300

    Orden de syntasix en repository

--- backend/api-inventario/src/main/java/com/panol/inventario/repository/InventarioRepository.java
+++ backend/api-inventario/src/main/java/com/panol/inventario/repository/InventarioRepository.java
@@ -1,7 +1,6 @@
 package com.panol.inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.panol.inventario.models.Inventario;
 
 public interface InventarioRepository extends JpaRepository<Inventario, Integer> {}
--- backend/api-inventario/src/main/java/com/panol/inventario/repository/MarcaRepository.java
+++ backend/api-inventario/src/main/java/com/panol/inventario/repository/MarcaRepository.java
@@ -1,7 +1,6 @@
 package com.panol.inventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.panol.inventario.models.Marca;
 
 public interface MarcaRepository extends JpaRepository<Marca, Integer> {}
