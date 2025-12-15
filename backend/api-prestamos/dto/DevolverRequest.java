a13473883b74f24c28675bff6d6876f3a8b82df3
Javier <javierparrareyes12345@gmail.com>
2025-12-15 20:10:39 -0300

    Eliminacion de archivos duplicados

--- backend/api-prestamos/dto/DevolverRequest.java
+++
@@ -1,9 +0,0 @@
-package com.panol.prestamos.dto;
-
-import jakarta.validation.constraints.*;
-
-public class DevolverRequest {
-  @NotNull public Integer id_detalle;
-  @NotNull @Min(1) public Integer cantidad;
-  @NotNull public Integer id_producto;
-}
--- backend/api-prestamos/dto/PrestarRequest.java
+++
@@ -1,9 +0,0 @@
-package com.panol.prestamos.dto;
-
-import jakarta.validation.constraints.*;
-
-public class PrestarRequest {
-  @NotNull public Integer id_prestamo;
-  @NotNull public Integer id_producto;
-  @NotNull @Min(1) public Integer cantidad;
-}
