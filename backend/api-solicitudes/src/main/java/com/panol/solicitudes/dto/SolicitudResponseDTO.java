c2bd88f3a203849a55b05964e7b473afa7d60df2
Javier <javierparrareyes12345@gmail.com>
2025-12-18 01:55:09 -0300

    Api-solicitudes terminada en funcionamiento

--- backend/api-solicitudes/src/main/java/com/panol/solicitudes/dto/SolicitudResponseDTO.java
+++ backend/api-solicitudes/src/main/java/com/panol/solicitudes/dto/SolicitudResponseDTO.java
@@ -1,17 +1,16 @@
 package com.panol.solicitudes.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import java.time.LocalDate;
-import java.util.List;
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+    import java.time.LocalDate;
+    import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class SolicitudResponseDTO {
+@EqualsAndHashCode(callSuper = false)
+public class SolicitudResponseDTO extends RepresentationModel<SolicitudResponseDTO> {
     private Integer id;
     private Integer rut;
     private String motivo;
--- backend/api-solicitudes/src/main/java/com/panol/solicitudes/service/SolicitudesService.java
+++ backend/api-solicitudes/src/main/java/com/panol/solicitudes/service/SolicitudesService.java
@@ -1,5 +1,6 @@
 package com.panol.solicitudes.service;
 
+import com.panol.solicitudes.dto.DetalleSolicitudDTO;
 import com.panol.solicitudes.dto.SolicitudRequestDTO;
 import com.panol.solicitudes.dto.SolicitudResponseDTO;
 import java.util.List;
@@ -10,4 +11,13 @@ public interface SolicitudesService {
     Map<String, Object> crearSolicitud(SolicitudRequestDTO req);
     void cambiarEstado(int id, String nuevoEstado);
     void eliminarSolicitud(int id);
+
+    //obtener por ID
+    SolicitudResponseDTO obtenerSolicitudPorId(int id);
+    // actualización completa (cuerpo y detalles)
+    SolicitudResponseDTO actualizarSolicitud(int id, SolicitudRequestDTO req);
+    // actualizar un detalle puntual
+    DetalleSolicitudDTO actualizarDetalle(int solicitudId, int detalleId, DetalleSolicitudDTO dto);
+    // eliminar un detalle puntual
+    void eliminarDetalle(int solicitudId, int detalleId);
 }
